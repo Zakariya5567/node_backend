eed49a2b1634ea3fb51c48e0140aac4ec47521e0
sardar saqib <sardarsaqib@codesorbit.com>
2025-11-05 00:31:20 +0500

    api handler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
-dotenv.config(); 
+dotenv.config({path:'./env'}); 
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 console.log("MONGO_URI:", process.env.MONGODB_URI); // 👈 should print your full URI
 
-connectDB();
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 3000,()=>{
+           console.log(`App is listening on port ${process.env.PORT}`)
+    })
+    console.log(`Database connection established`)
+}).catch((error)=>{   
+    console.log(`Error on database connection`)
+})
+ 
